56f3d4c07ca53913d8452e1ccb647f7241f16ae0
Tommy Ngoy <tommy.ngoy@outlook.com>
2021-06-09 15:48:29 -0400

    create computerPlay function

---
+++ main.js
@@ -0,0 +1,14 @@
+const options = ["Rock", "Paper", "Scissors"]
+
+function computerPlay(){
+    let choice = Math.floor(Math.random() * options.length); 
+    return options[choice];
+}
+
+function playerSelection(){
+
+}
+
+function playRound(){
+
+}
