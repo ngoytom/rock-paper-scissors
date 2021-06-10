35b434a7f5ffdf5cce73f70ebe17145b73273304
Tommy Ngoy <tommy.ngoy@outlook.com>
2021-06-10 00:00:11 -0400

    modular arithmetic to calc winner

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
-const options = ["Rock", "Paper", "Scissors"]
+const options = [0, 1, 2]
 
 function computerPlay(){
     let choice = Math.floor(Math.random() * options.length); 
     return options[choice];
 }
 
-function playerSelection(){
+function playRound(clicked_id){
+    let playerChoice = clicked_id;
+    let computerChoice = computerPlay();
+
+    if (playerChoice == "rock"){
+        playerChoice = 0;
+    }
+    else if (playerChoice == "paper"){
+        playerChoice = 1;
+    }
+    else if (playerChoice == "scissors"){
+        playerChoice = 2;
+    }
+    let winner = calcWinner(playerChoice, computerChoice);
+    console.log(computerChoice);
+    console.log(winner);
 
 }
 
-function playRound(){
-
+/**
+ * Using modular arithmetic to calculate winner. Greater by 1 
+ * @param {*} playerChoice 
+ * @param {*} compouterChoice 
+ * @returns 0 if computer wins, 1 if tie, 2 if player one wins
+ */
+function calcWinner(playerChoice, computerChoice){
+    let mod = (playerChoice+1) % 3;
+    if (mod == computerChoice){
+        return "computer";
+    }
+    else if (playerChoice == computerChoice){
+        return "tie";
+    }
+    else{
+        return "player";
+    }
 }
