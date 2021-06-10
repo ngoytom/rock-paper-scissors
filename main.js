fb65d04c3b5fc78e0d03f26ebfd0a70afc5ba097
Tommy Ngoy <tommy.ngoy@outlook.com>
2021-06-10 00:28:56 -0400

    display choice on gui

--- main.js
+++ main.js
@@ -11,17 +11,28 @@ function playRound(clicked_id){
 
     if (playerChoice == "rock"){
         playerChoice = 0;
+        document.getElementById("playerchoice").src = "https://lh3.googleusercontent.com/proxy/BQ8riHH5dQTwXK_dv5sk5rG-6TSmDTRTX9mmNRIG83mhZCH_YBBY_F4G403HZ7e6RCrLIFVlwnW6NfUesinK2DGDCOrrktU";
     }
     else if (playerChoice == "paper"){
         playerChoice = 1;
+        document.getElementById("playerchoice").src = "https://lh3.googleusercontent.com/proxy/yEzI3qFgTuwUNPmH8p66NCPgd1Wb5PHd9ZKypFiiaGpG5EwmTeGM2cA3YSm3wvJfR96Wwuv09ZIvKnIfGKyRT7yotzXP1kA";
     }
     else if (playerChoice == "scissors"){
         playerChoice = 2;
+        document.getElementById("playerchoice").src = "http://www.clker.com/cliparts/7/d/N/6/X/o/scissor-hand.svg";
+    }
+    
+    if (computerChoice == 0){
+        document.getElementById("computerchoice").src = "https://lh3.googleusercontent.com/proxy/BQ8riHH5dQTwXK_dv5sk5rG-6TSmDTRTX9mmNRIG83mhZCH_YBBY_F4G403HZ7e6RCrLIFVlwnW6NfUesinK2DGDCOrrktU";
+    }
+    else if (computerChoice == 1){
+        document.getElementById("computerchoice").src = "https://lh3.googleusercontent.com/proxy/yEzI3qFgTuwUNPmH8p66NCPgd1Wb5PHd9ZKypFiiaGpG5EwmTeGM2cA3YSm3wvJfR96Wwuv09ZIvKnIfGKyRT7yotzXP1kA";
+    }
+    else if (computerChoice == 2){
+        document.getElementById("computerchoice").src = "http://www.clker.com/cliparts/7/d/N/6/X/o/scissor-hand.svg";
     }
     let winner = calcWinner(playerChoice, computerChoice);
-    console.log(computerChoice);
     console.log(winner);
-
 }
 
 /**
@@ -33,12 +44,12 @@ function playRound(clicked_id){
 function calcWinner(playerChoice, computerChoice){
     let mod = (playerChoice+1) % 3;
     if (mod == computerChoice){
-        return "computer";
+        return 0;
     }
     else if (playerChoice == computerChoice){
-        return "tie";
+        return 1;
     }
     else{
-        return "player";
+        return 2;
     }
 }
